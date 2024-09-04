357981ef57424cf6c41d791fae54ef5b4beebe95
Gabriel <gabrielsouza65115@gmail.com>
2024-09-04 17:07:42 -0300

    feat: Import o componente FeaturesWithIcons do template

--- src/components/sections/FeaturesWithIcons.jsx
+++ src/components/sections/FeaturesWithIcons.jsx
@@ -12,66 +12,67 @@ import Icon4 from "../../assets/imgs/icons/icon4.webp";
 
 export default function FeaturesWithIcons() {
   return (
-    <SectionArea id="service" className="" paddingbot={false}>
-      <MotionDivDownToUp className="w-full flex justify-center">
-        <SectionHeader
-          className="text-center"
-          miniTitle={content.texts.features.miniTag}
-          sectionHeaderTitle={content.texts.features.title}
-          sectionHeaderSubtitle={content.texts.features.subtitle}
-          color="dark"
-        />
-      </MotionDivDownToUp>
+    <SectionArea id="service" className="squares">
+      <SectionHeader
+        className="text-center"
+        miniTitle={content.texts.features.miniTag}
+        sectionHeaderTitle={content.texts.features.title}
+        sectionHeaderSubtitle={content.texts.features.subtitle}
+        color="dark"
+      />
+
       <SectionWrapper>
-        <div className="flex flex-col items-center justify-between w-full tablet1:flex-row">
-          <div className="col1">
+        <div className="flex flex-col items-center justify-evenly w-full tablet1:flex-row">
+          <div className="col1 desktop1:w-[28%] flex flex-col items-center">
             <MotionDivDownToUp>
               <IconFeatureCard
-                icon={<img src={Icon1} alt="" />}
+                icon={content.texts.features.card1.svgIcon}
                 title={content.texts.features.card1.title}
                 paragraph={content.texts.features.card1.subtitle}
-                className="tablet1:mb-[46px]"
+                className="tablet1:mb-[26px] desktop1:mb-0 desktop2:mb-[46px]"
               />
             </MotionDivDownToUp>
             <MotionDivDownToUp>
               <IconFeatureCard
-                icon={<img src={Icon2} alt="" />}
+                icon={content.texts.features.card2.svgIcon}
                 title={content.texts.features.card2.title}
                 paragraph={content.texts.features.card2.subtitle}
               />
             </MotionDivDownToUp>
           </div>
-          <MotionDivDownToUp className="w-full flex justify-center">
-            <div className="hidden desktop1:flex justify-center w-full col2">
-              <img
-                src={content.texts.features.imgFeatures}
-                alt="Imagem ilustrativa da seção"
-              />
+
+          <MotionDivDownToUp className="hidden desktop1:flex justify-center w-[32%]">
+            <div
+              className="hidden h-[640px] w-full desktop1:flex col2 rounded-2xl bg-top bg-cover"
+              style={{
+                backgroundImage: `url(${content.texts.features.imgFeatures})`,
+              }}
+            >
+              {/* <img
+              src={content.texts.features.imgFeatures}
+              alt="Imagem ilustrativa da seção"
+              className="rounded-2xl"
+            /> */}
             </div>
           </MotionDivDownToUp>
-          <div className="col3 ">
-            <MotionDivDownToUp>
+          <div className="col3 desktop1:w-[28%] flex flex-col items-center">
+            <MotionDivDownToUp className=" flex items-center w-full tablet1:w-[290px] desktop1:w-[250px] ">
               <IconFeatureCard
-                icon={<img src={Icon3} alt="" />}
+                icon={content.texts.features.card3.svgIcon}
                 title={content.texts.features.card3.title}
                 paragraph={content.texts.features.card3.subtitle}
-                className="tablet1:mb-[46px]"
+                className="tablet1:mb-[26px] desktop1:mb-0 desktop2:mb-[46px]"
               />
             </MotionDivDownToUp>
-            <MotionDivDownToUp>
+            <MotionDivDownToUp className=" flex items-center w-full tablet1:w-[290px] desktop1:w-[250px] ">
               <IconFeatureCard
-                icon={<img src={Icon4} alt="" />}
+                icon={content.texts.features.card4.svgIcon}
                 title={content.texts.features.card4.title}
                 paragraph={content.texts.features.card4.subtitle}
               />
             </MotionDivDownToUp>
           </div>
         </div>
-        <img
-          src={content.texts.features.imgDivisor}
-          className="p-[84px]"
-          alt="Imagem de divisão em formato de onda"
-        />
       </SectionWrapper>
     </SectionArea>
   );
